c660c126710bcd219421b5643e9e78dfdbc85ce0
Bartosz Szczepański <leukocyty@gmail.com>
2022-04-30 13:53:10 +0200

    structure &styling of homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  lang = "pl";
+
+  constructor(private route: ActivatedRoute, private router: Router) {
+  }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      if(params['lang'] != null){
+        this.lang = params['lang'];
+      }
+    });
+  }
+
+  changeLang(changeTo: string)
+  {
+    this.router.navigate(['home'], { queryParams: { lang: changeTo } });
   }
 
 }
